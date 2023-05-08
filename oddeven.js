b5d980fec5c69800360cc0aaeb626b2c3e1fa8ac
Chandik . v . r <132890000+th4n4t0z@users.noreply.github.com>
2023-05-08 16:01:17 +0530

    Add files via upload

---
+++ oddeven.js
@@ -0,0 +1,11 @@
+let arr = [2,3,5,6,8,11]
+
+for (let index = 0; index < arr.length; index++) {
+    if(arr [index]%2==0){
+   console.log(`${arr [index]} is even`);
+    }
+   else{
+       
+    console.log(`${arr [index]} is odd`);
+    }
+}
