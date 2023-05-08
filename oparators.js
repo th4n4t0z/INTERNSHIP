4108f4cde157f32354985d08d5d7f54c50369707
Chandik . v . r <132890000+th4n4t0z@users.noreply.github.com>
2023-05-08 14:29:40 +0530

    Add files via upload

---
+++ oparators.js
@@ -0,0 +1,8 @@
+const person = {
+    name : 'oz',
+    age : 19,
+    place :'null'
+};
+    //console.log(person.place)
+    let v = `my name is ${person.name}`;
+    console.log(v) 
---
+++ script.js
@@ -0,0 +1,6 @@
+//console.log("my name is Chandik");
+//console.log("youuuuuu....!");
+let frame="you again...!"
+console.log(frame);
+let fram="again  and again...!"
+console.log(fram);
